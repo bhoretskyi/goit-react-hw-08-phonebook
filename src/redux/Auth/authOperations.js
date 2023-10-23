472d4de8e7765f34ac066496a49975f71e15506f
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-10-23 18:12:15 +0200

    edit auth operations

--- src/redux/Auth/authOperations.js
+++ src/redux/Auth/authOperations.js
@@ -14,7 +14,7 @@ export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkApi) => {
     try {
-      const res = await axios.post('/contacts', credentials);
+      const res = await axios.post('/users/signup', credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -22,3 +22,25 @@ export const register = createAsyncThunk(
     }
   }
 );
+
+export const logIn = createAsyncThunk(
+  'auth/logIn',
+  async (credentials, thunkApi) => {
+    try {
+      const res = await axios.post('/users/login', credentials);
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logOut', async (_, thunkApi) => {
+  try {
+    const res = await axios.post('users/logout');
+    clearAuthHeader();
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.message);
+  }
+});
