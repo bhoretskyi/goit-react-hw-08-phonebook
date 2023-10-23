23ce12c350b0349739c35c47ada51c59b15c0c0b
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-10-23 18:23:25 +0200

    add refresh

--- src/redux/Auth/authOperations.js
+++ src/redux/Auth/authOperations.js
@@ -44,3 +44,23 @@ export const logOut = createAsyncThunk('auth/logOut', async (_, thunkApi) => {
     return thunkApi.rejectWithValue(error.message);
   }
 });
+
+
+export const refreshUser = createAsyncThunk(
+    'auth/refresh',
+    async(_, thunkApi) => {
+        const state = thunkApi.getState()
+        const persistedToken = state.auth.token
+        if(persistedToken === null) {
+            return thunkApi.rejectWithValue('unable to catch user')
+        }
+        try{
+            setAuthHeader(persistedToken)
+            const res = await axios.get('users/current')
+            return res.data
+        }
+        catch (error) {
+            return thunkApi.rejectWithValue(error.message);
+          }
+    }
+)
